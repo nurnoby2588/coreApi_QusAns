7b401d32922857cd1ac487273cbaeca85a9125b5
nurnoby admin <nurnoby323@gmail.com>
2025-02-11 15:11:21 +0600

    get api emplement

---
+++ Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coreApi_QusAns.Controllers
+{
+    [Route("/api/[controller]/[Action]")]
+    public class TestController : Controller
+    {
+        [HttpGet (Name ="QtyProjectName")]
+        public IActionResult QtyProjectName()
+        {
+            return Ok("success");
+        }
+        [HttpGet(Name ="employName")]
+
+        public IActionResult employName()
+        {
+            return Ok( new
+            {
+                name = "nurnoby" , 
+                status= "success"
+            });
+        }
+
+    }
+}
